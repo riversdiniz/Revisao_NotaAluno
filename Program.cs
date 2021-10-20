c685d2a326d5b4f8176148fa6bbdac98a4c5976c
River Diniz <river-s-diniz@hotmail.com>
2021-10-20 19:32:27 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("INFORME O NOME DO ALUNO:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("INFORME A NOTA DO ALUNO:");
+
+                         if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                         {
+                            aluno.Nota = nota;
+                         }
+                         else
+                         {
+                             throw new ArgumentException("VALOR DA NOTA DEVE SER DECIMAL");
+                         }
+
+                         alunos[indiceAluno] = aluno;
+                         indiceAluno++;
+
+                        break;
+                    
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                            
+                               Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                               notaTotal = notaTotal + alunos[i].Nota;
+                               nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if(mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+               
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("INFORME A OPÇÃO DESEJADA:");
+            Console.WriteLine(" 1 - INSERIR NOVO ALUNO");
+            Console.WriteLine(" 2 - LISTAR ALUNOS");
+            Console.WriteLine(" 3 - CALCULAR MÉDIA GERAL");
+            Console.WriteLine(" X - SAIR");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
